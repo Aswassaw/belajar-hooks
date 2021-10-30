eb1bf5982212622ec932d6f5dffbe9d59f7af8ce
Andry Pebrianto <andrypeb227@gmail.com>
2021-10-30 12:46:19 +0700

    Conditionally run effects with second array parameter on useEffect

--- src/components/2. useEffect/HookCounter.jsx
+++ src/components/2. useEffect/HookCounter.jsx
@@ -2,8 +2,11 @@ import React, { useEffect, useState } from "react";
 
 const HookCounter = () => {
   const [counter, setCounter] = useState(0);
+  const [name, setName] = useState("");
 
   useEffect(() => {
+    console.log("useEffect executed");
+
     document.title = counter;
   }, [counter]);
 
@@ -12,6 +15,12 @@ const HookCounter = () => {
       <button onClick={() => setCounter((val) => val + 1)}>
         {counter} - Increment
       </button>
+      <br />
+      <input
+        type='text'
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
     </div>
   );
 };
