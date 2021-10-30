ad459d7c94876869a8d9e8fb8afb2e2203d8e206
Andry Pebrianto <andrypeb227@gmail.com>
2021-10-30 11:51:24 +0700

    useState with object

--- src/components/1. useState/HookCounter.jsx
+++ src/components/1. useState/HookCounter.jsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 const HookCounter = () => {
   const initialCount = 0;
   const [counter, setCounter] = useState(initialCount);
+  const [name, setName] = useState({ firstName: "", lastName: "" });
 
   const customIncrement = (incrementVal) => {
     for (let i = 0; i < incrementVal; i++) {
@@ -10,14 +11,44 @@ const HookCounter = () => {
     }
   };
 
+  const onChangeHandler = (e) => {
+    setName({
+      ...name,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
     <div>
-      <strong>{counter}</strong>
+      <div>
+        <strong>{counter}</strong>
+        <br />
+        <button onClick={() => setCounter(counter + 1)}>Tambah</button>
+        <button onClick={() => setCounter(counter - 1)}>Kurang</button>
+        <button onClick={() => setCounter(initialCount)}>Reset</button>
+        <button onClick={() => customIncrement(5)}>Tambah 5</button>
+      </div>
+
       <br />
-      <button onClick={() => setCounter(counter + 1)}>Tambah</button>
-      <button onClick={() => setCounter(counter - 1)}>Kurang</button>
-      <button onClick={() => setCounter(initialCount)}>Reset</button>
-      <button onClick={() => customIncrement(5)}>Tambah 5</button>
+
+      <div>
+        <strong>{name.firstName + " " + name.lastName}</strong>
+        <br />
+        <input
+          type='text'
+          name='firstName'
+          placeholder='First Name'
+          value={name.firstName}
+          onChange={onChangeHandler}
+        />
+        <input
+          type='text'
+          name='lastName'
+          placeholder='Last Name'
+          value={name.lastName}
+          onChange={onChangeHandler}
+        />
+      </div>
     </div>
   );
 };
