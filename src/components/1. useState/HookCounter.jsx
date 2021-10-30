be970a4c4d97d0be11a78f4457f3a8438e4ec4f5
Andry Pebrianto <andrypeb227@gmail.com>
2021-10-30 11:34:41 +0700

    useState with previous state

--- src/components/1. useState/HookCounter.jsx
+++ src/components/1. useState/HookCounter.jsx
@@ -1,7 +1,14 @@
 import React, { useState } from "react";
 
 const HookCounter = () => {
-  const [counter, setCounter] = useState(0);
+  const initialCount = 0;
+  const [counter, setCounter] = useState(initialCount);
+
+  const customIncrement = (incrementVal) => {
+    for (let i = 0; i < incrementVal; i++) {
+      setCounter((val) => val + 1);
+    }
+  };
 
   return (
     <div>
@@ -9,6 +16,8 @@ const HookCounter = () => {
       <br />
       <button onClick={() => setCounter(counter + 1)}>Tambah</button>
       <button onClick={() => setCounter(counter - 1)}>Kurang</button>
+      <button onClick={() => setCounter(initialCount)}>Reset</button>
+      <button onClick={() => customIncrement(5)}>Tambah 5</button>
     </div>
   );
 };
