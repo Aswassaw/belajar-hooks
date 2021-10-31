e3894be8f092506ab62917321708fa114dfbc4fe
Andry Pebrianto <andrypeb227@gmail.com>
2021-11-01 03:50:46 +0700

    useReducer (simple & action)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
 // import Hook1 from "./components/1. useState";
 // import Hook2 from "./components/2. useEffect";
-import Hook3 from "./components/3. useContext";
+// import Hook3 from "./components/3. useContext";
+import Hook4 from "./components/4. useReducer";
 
 function App() {
   return (
     <div className='App'>
-      <Hook3 />
+      <Hook4 />
     </div>
   );
 }
---
+++ src/components/4. useReducer/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CounterOne from "./CounterOne";
+
+const index = () => {
+  return (
+    <div>
+      <CounterOne />
+    </div>
+  );
+};
+
+export default index;
