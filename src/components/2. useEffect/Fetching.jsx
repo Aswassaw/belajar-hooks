2735cebc3b39cbfeed12eb4f4b59c6cb98ef6b7b
Andry Pebrianto <andrypeb227@gmail.com>
2021-10-31 16:46:54 +0700

    Fetching data with useEffect Part 2

--- src/components/2. useEffect/Fetching.jsx
+++ src/components/2. useEffect/Fetching.jsx
@@ -3,9 +3,30 @@ import axios from "axios";
 
 const Fetching = () => {
   const [users, setUsers] = useState([]);
+  const [userId, setUserId] = useState("1");
+  const [user, setUser] = useState({});
+
   useEffect(() => {
     fetchUsers();
   }, []);
+
+  useEffect(() => {
+    const fetchUsersById = async () => {
+      try {
+        const id = userId.trim() || 1;
+        const { data } = await axios.get(
+          "https://jsonplaceholder.typicode.com/users/" + id
+        );
+        setUser(data);
+        document.getElementById("status").innerHTML = "Fetch Success";
+      } catch (error) {
+        console.log(error.message);
+        document.getElementById("status").innerHTML = error.message;
+      }
+    };
+
+    fetchUsersById();
+  }, [userId]);
 
   const fetchUsers = async () => {
     try {
@@ -15,7 +36,6 @@ const Fetching = () => {
       setUsers(data);
     } catch (error) {
       console.log(error.message);
-      alert(error.message);
     }
   };
 
@@ -25,6 +45,28 @@ const Fetching = () => {
         {users.map((user) => (
           <li key={user.id}>{user.name}</li>
         ))}
+        <hr />
+        <input
+          type='text'
+          value={userId}
+          onChange={(e) => setUserId(e.target.value)}
+        />
+        <hr />
+        <div id='status'></div>
+        <hr />
+        {user && (
+          <div>
+            <p>
+              Name: <strong>{user.name}</strong>
+            </p>
+            <p>
+              Email: <strong>{user.email}</strong>
+            </p>
+            <p>
+              Phone: <strong>{user.phone}</strong>
+            </p>
+          </div>
+        )}
       </ul>
     </div>
   );
