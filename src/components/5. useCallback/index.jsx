c26ad590cc3bba67fbdd12342385c25cd11b3ac4
Andry Pebrianto <andrypeb227@gmail.com>
2021-11-01 15:49:48 +0700

    26. useCallback Hook

---
+++ src/components/5. useCallback/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ParentComponent from "./ParentComponent";
+
+const index = () => {
+  return (
+    <div>
+      <ParentComponent />
+    </div>
+  );
+};
+
+export default index;
